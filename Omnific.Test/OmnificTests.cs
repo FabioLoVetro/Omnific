3200480226be7e7c27f87c5b67bcdfb93ce2e979
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-13 16:57:04 +0200

    Add project files.

---
+++ Omnific.Test/OmnificTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+namespace Omnific.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
