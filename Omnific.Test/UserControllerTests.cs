65a12645ff74c9cc9ce6624de0b98f68f1b62090
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-14 18:07:31 +0200

    Fabio - added or adapted tests for CreateUserController and GetUsers_Returns_AllUsers methods

--- Omnific.Test/UserControllerTests.cs
+++ Omnific.Test/UserControllerTests.cs
@@ -5,6 +5,7 @@ using Omnific.Services;
 using FluentAssertions;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 
 namespace Omnific.Test
 {
@@ -12,6 +13,15 @@ namespace Omnific.Test
     {
         private Mock<IUserService> _mockUserService;
         private UserController _userController;
+        private static List<User> GetTestUsers()
+        {
+            return new List<User>
+        {
+            new User("User1","User1@gmail.com","PWUser1"),
+            new User("User2","User2@gmail.com","PWUser2"),
+            new User("User3","User3@gmail.com","PWUser3"),
+        };
+        }
 
         [SetUp]
         public void Setup()
@@ -24,10 +34,12 @@ namespace Omnific.Test
         public void CreateUser_Should_Return_A_User()
         {
             //Arrage
-            _mockUserService.Setup(b => b.CreateNewUser()).Returns(new User());
+            User user = new User("Paz", "pazsoangara@gmail.com", "pazzy");
+
+            _mockUserService.Setup(b => b.CreateNewUserService("","","")).Returns(user);
 
             //Act
-            var newUser = _userController.CreateUser("Paz", "pazsonagara@gmail.com", "pazzy");
+            var newUser = _userController.CreateUserController("", "", "");
 
             //Assert
             newUser.Should().BeOfType(typeof(ActionResult<User>));
@@ -37,21 +49,43 @@ namespace Omnific.Test
         public void CreateUser_Should_Return_A_User_With_API_Key()
         {
             //Arrange
-            User user = new User();
-            user.UserName = "Paz";
-            user.Email = "pazsoangara@gmail.com";
-            user.Password = "pazzy";
-            user.ApiKey = "someAPIKey";
+            User user = new User("Paz", "pazsoangara@gmail.com", "pazzy");
 
-            _mockUserService.Setup(us => us.CreateNewUser()).Returns(user);
+            _mockUserService.Setup(userService => userService.CreateNewUserService("", "", "")).Returns(user);
 
             //Act
-            var newuser = _userController.CreateUser("Paz", "pazsonagara@gmail.com", "pazzy");
+            var newuser = _userController.CreateUserController("", "", "");
 
             //Assert
-            newuser.Value.ApiKey.Should().Be("someAPIKey");
+            newuser.Value.Id.Should().Be(user.Id);
+            newuser.Value.ApiKey.Should().Be(user.ApiKey);
+            newuser.Value.UserName.Should().Be("Paz");
+            newuser.Value.Email.Should().Be("pazsoangara@gmail.com");
+            newuser.Value.Password.Should().Be("pazzy");
 
+        }
 
+        [Test]
+        public void GetUsers_Returns_AllUsers()
+        {
+            //Arange
+            _mockUserService.Setup(b => b.GetAllUsersService()).Returns(GetTestUsers());
+
+            //Act
+            var result = _userController.GetAllUsersController();
+
+            //Assert
+            result.Should().BeOfType(typeof(ActionResult<IEnumerable<User>>));
+            result.Value.ElementAt(0).UserName.Should().BeEquivalentTo(GetTestUsers().ElementAt(0).UserName);
+            result.Value.ElementAt(0).Email.Should().BeEquivalentTo(GetTestUsers().ElementAt(0).Email);
+            result.Value.ElementAt(0).Password.Should().BeEquivalentTo(GetTestUsers().ElementAt(0).Password);
+            result.Value.ElementAt(1).UserName.Should().BeEquivalentTo(GetTestUsers().ElementAt(1).UserName);
+            result.Value.ElementAt(1).Email.Should().BeEquivalentTo(GetTestUsers().ElementAt(1).Email);
+            result.Value.ElementAt(1).Password.Should().BeEquivalentTo(GetTestUsers().ElementAt(1).Password);
+            result.Value.ElementAt(2).UserName.Should().BeEquivalentTo(GetTestUsers().ElementAt(2).UserName);
+            result.Value.ElementAt(2).Email.Should().BeEquivalentTo(GetTestUsers().ElementAt(2).Email);
+            result.Value.ElementAt(2).Password.Should().BeEquivalentTo(GetTestUsers().ElementAt(2).Password);
+            result.Value.Count().Should().Be(3);
         }
     }
 }
