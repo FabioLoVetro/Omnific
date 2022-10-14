fdf99442d58eafcd5ad0bc83f532825e0ec8b0f9
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-14 18:02:13 +0200

    Fabio - added OmnificContext

---
+++ Omnific/Model/OmnificContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System;
+using Omnific.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Omnific.Model
+{
+    public class OmnificContext : DbContext
+    {
+        public OmnificContext(DbContextOptions<OmnificContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
