6c64a4f4cbcb8ba1b73e66cc19ad99bfcdd75d79
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-14 18:04:37 +0200

    Fabio - added properties for swagger and MeroryDatabase

--- Omnific/Program.cs
+++ Omnific/Program.cs
@@ -1,25 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Omnific.Model;
+using Omnific.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<OmnificContext>(option =>
+    option.UseInMemoryDatabase("OmnificDB"));
+
+// Configure Swagger/OpenAPI Documentation
+// You can learn more on this link: https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+//app.UseStaticFiles();
 
-app.UseRouting();
+//app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
+
+//app.MapRazorPages();
 
 app.Run();
