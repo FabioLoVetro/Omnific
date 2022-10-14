3b69c41c1ef1e70856f3d4cdce8c0088f8dd95b6
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-14 18:02:40 +0200

    Fabio - added Type for User model

---
+++ Omnific/Model/Type.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Omnific.Model
+{
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public enum Type
+        {
+            Viewer,
+            Creator,
+            Administrator
+        }
+}
