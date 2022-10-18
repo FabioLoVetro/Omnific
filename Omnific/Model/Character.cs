34dc76fb9c992f24ca2cb358798a7acb4c8c87c2
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-18 19:11:18 +0200

    Fabio - Added new classes Character and FantasyCharacter

---
+++ Omnific/Model/Character.cs
@@ -0,0 +1,29 @@
+﻿namespace Omnific.Model
+{
+    public abstract class Character
+    {
+        public int Id { get; set; }
+        public string APIKeyInventor { get; set; }
+        public string Name { get; set; }
+        public double Hight { get; set; }
+        public double Weight { get; set; }
+        public string Habitat { get; set; }
+        public string Description { get; set; }
+        public string CharacterBase { get; set; }
+        public string PictureURL { get; set; }
+        public string Powers { get; set; }
+
+        public Character(string name, double hight, double weight, string habitat, string description, string characterBase, string pictureURL, string powers)
+        {
+            APIKeyInventor = "UID-APIK";
+            Name = name;
+            Hight = hight;
+            Weight = weight;
+            Habitat = habitat;
+            Description = description;
+            CharacterBase = characterBase;
+            PictureURL = pictureURL;
+            Powers = powers;
+        }
+    }
+}
---
+++ Omnific/Model/FantasyCharacter.cs
@@ -0,0 +1,9 @@
+﻿namespace Omnific.Model
+{
+    public class FantasyCharacter : Character
+    {
+        public FantasyCharacter(string name, double hight, double weight, string habitat, string description, string characterBase, string pictureURL, string powers) : base(name, hight, weight, habitat, description, characterBase, pictureURL, powers)
+        {
+        }
+    }
+}
