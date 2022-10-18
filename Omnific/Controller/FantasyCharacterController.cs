009ec49ce60e1b47b4575a36e1740b8e65c577b5
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-18 19:12:03 +0200

    Fabio - Added controller for FantasyCharacter

---
+++ Omnific/Controller/FantasyCharacterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Omnific.Model;
+using Omnific.Services;
+
+namespace Omnific.Controller
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class FantasyCharacterController : ControllerBase
+    {
+        private IFantasyCharacterService _iFantasyCharacterService;
+
+        public FantasyCharacterController(IFantasyCharacterService iFantasyCharacter) {
+            _iFantasyCharacterService = iFantasyCharacter;
+        }
+
+        [HttpPost]
+        public ActionResult<FantasyCharacter> CreateFantasyCharacterController(string name, double hight, double weight, string habitat, string description, string characterBase, string pictureURL, string powers)
+        {
+            return _iFantasyCharacterService.CreateNewFantasyCharacter(name, hight, weight, habitat, description, characterBase, pictureURL, powers);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<FantasyCharacter>> GetAllFantasyCharactersController()
+        {
+            return _iFantasyCharacterService.GetAllFantasyCharacters();
+        }
+    }
+}
