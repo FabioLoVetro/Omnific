a7ea7074f6e95df000800f81b89042aaad6245a7
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-18 19:12:41 +0200

    Fabio - Added service for FantasyCharacter

---
+++ Omnific/Services/IFantasyCharacterService.cs
@@ -0,0 +1,10 @@
+﻿using Omnific.Model;
+
+namespace Omnific.Services
+{
+    public interface IFantasyCharacterService
+    {
+        public FantasyCharacter CreateNewFantasyCharacter(string name, double hight, double weight, string habitat, string description, string characterBase, string pictureURL, string powers);
+        public List<FantasyCharacter> GetAllFantasyCharacters();
+    }
+}
