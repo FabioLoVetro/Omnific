342268efbd568a180569900e14fd835147592095
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-21 12:18:06 +0200

    Fabio - Added methods Create, Update, Delete, Gets... for User. Added comments

--- Omnific/Services/IUserService.cs
+++ Omnific/Services/IUserService.cs
@@ -5,6 +5,12 @@ namespace Omnific.Services
     public interface IUserService
     {
         public User CreateNewUser(string username, string email, string password);
+        public User? UpdateUserById(int id, string newUserName, string newEMail, string newPassword);
+        public User? GetUserById(int id);
+        public User? GetUserByApiKey(string ApiKey);
+        public User? GetUserByUsername(string username);
+        public User? GetUserByEmail(string email);
         public List<User> GetAllUsers();
+        public User? DeleteUserById(int id);
     }
 }
