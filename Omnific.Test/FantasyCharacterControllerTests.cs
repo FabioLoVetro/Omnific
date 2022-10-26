1d4d722bd58570222a5f53abac81e113f73b3b88
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-26 17:38:19 +0200

    Fabio - implementation FantasyCharacterControllerTests

--- Omnific.Test/FantasyCharacterControllerTests.cs
+++ Omnific.Test/FantasyCharacterControllerTests.cs
@@ -5,11 +5,6 @@ using NUnit.Framework;
 using Omnific.Controller;
 using Omnific.Model;
 using Omnific.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Omnific.Test
 {
@@ -17,26 +12,101 @@ namespace Omnific.Test
     {
         private Mock<IFantasyCharacterService> _mockFantasyCharacterService;
         private FantasyCharacterController _fantasyCharacterController;
+        private FantasyCharacter _fantasyCharacter;
+        private List<FantasyCharacter> _Characters;
 
         [SetUp]
         public void Setup()
         {
             _mockFantasyCharacterService = new Mock<IFantasyCharacterService>();
             _fantasyCharacterController = new FantasyCharacterController(_mockFantasyCharacterService.Object);
+            _fantasyCharacter = new FantasyCharacter("Magician", 1.70, 80, "Earth", "Magician", "www.omnific.com/fantasycharacter/1", "12345678", "Magic");
+            _Characters = new List<FantasyCharacter> { _fantasyCharacter };
         }
 
         [Test]
         public void CreateFantasyCharacter_Should_Return_A_FantasyCharacter()
         {
             //Arrage
-            var fantasyCharacter = new FantasyCharacter("",0, 0, "", "", "", "","");
-            _mockFantasyCharacterService.Setup(b => b.CreateNewFantasyCharacter("", 0, 0, "", "", "", "")).Returns(fantasyCharacter);
-
+            _mockFantasyCharacterService.Setup(b => b.CreateNewFantasyCharacter("", 0, 0, "", "", "", "","")).Returns(_fantasyCharacter);
             //Act
-            var newFantasyCharacter = _fantasyCharacterController.CreateFantasyCharacterController("", 0, 0, "", "", "", "");
-
+            var newFantasyCharacter = _fantasyCharacterController.CreateFantasyCharacter("", 0, 0, "", "", "", "","");
             //Assert
+            newFantasyCharacter.Should().NotBeNull();
             newFantasyCharacter.Should().BeOfType(typeof(ActionResult<FantasyCharacter>));
+            newFantasyCharacter.Value.Should().BeEquivalentTo(_fantasyCharacter);
+        }
+        [Test]
+        public void GetAllFantasyCharacter()
+        {
+            //Arrage
+            _mockFantasyCharacterService.Setup(b => b.GetAllFantasyCharacter()).Returns(_Characters);
+            //Act
+            var newFantasyCharacter = _fantasyCharacterController.GetAllFantasyCharacter();
+            //Assert
+            newFantasyCharacter.Should().NotBeNull();
+            newFantasyCharacter.Should().BeOfType(typeof(ActionResult<IEnumerable<FantasyCharacter>>));
+            newFantasyCharacter.Value.Should().BeEquivalentTo(_Characters);
+        }
+        [Test]
+        public void GetFantasyCharacterById()
+        {
+            //Arrage
+            _mockFantasyCharacterService.Setup(b => b.GetFantasyCharacterById(1)).Returns(_fantasyCharacter);
+            //Act
+            var newFantasyCharacter = _fantasyCharacterController.GetFantasyCharacterById(1);
+            //Assert
+            newFantasyCharacter.Should().NotBeNull();
+            newFantasyCharacter.Should().BeOfType(typeof(ActionResult<FantasyCharacter>));
+            newFantasyCharacter.Value.Should().BeEquivalentTo(_fantasyCharacter);
+        }
+        [Test]
+        public void GetFantasyCharacterByAPIKeyInventor()
+        {
+            //Arrage
+            _mockFantasyCharacterService.Setup(b => b.GetFantasyCharacterByAPIKeyInventor("")).Returns(_Characters);
+            //Act
+            var newFantasyCharacter = _fantasyCharacterController.GetFantasyCharacterByAPIKeyInventor("");
+            //Assert
+            newFantasyCharacter.Should().NotBeNull();
+            newFantasyCharacter.Should().BeOfType(typeof(ActionResult<IEnumerable<FantasyCharacter>>));
+            newFantasyCharacter.Value.Should().BeEquivalentTo(_Characters);
+        }
+        [Test]
+        public void GetFantasyCharacterByName()
+        {
+            //Arrage
+            _mockFantasyCharacterService.Setup(b => b.GetFantasyCharacterByName("")).Returns(_Characters);
+            //Act
+            var newFantasyCharacter = _fantasyCharacterController.GetFantasyCharacterByName("");
+            //Assert
+            newFantasyCharacter.Should().NotBeNull();
+            newFantasyCharacter.Should().BeOfType(typeof(ActionResult<IEnumerable<FantasyCharacter>>));
+            newFantasyCharacter.Value.Should().BeEquivalentTo(_Characters);
+        }
+        [Test]
+        public void UpdateFantasyCharacterById()
+        {
+            //Arrage
+            _mockFantasyCharacterService.Setup(b => b.UpdateFantasyCharacterById(1, "", 0, 0, "", "", "", "","")).Returns(_fantasyCharacter);
+            //Act
+            var updatedFantasyCharacter = _fantasyCharacterController.UpdateFantasyCharacterById(1, "", 0, 0, "", "", "", "","");
+            //Assert
+            updatedFantasyCharacter.Should().NotBeNull();
+            updatedFantasyCharacter.Should().BeOfType(typeof(ActionResult<FantasyCharacter>));
+            updatedFantasyCharacter.Value.Should().BeEquivalentTo(_fantasyCharacter);
+        }
+        [Test]
+        public void DeleteFantasyCharacterById()
+        {
+            //Arrage
+            _mockFantasyCharacterService.Setup(b => b.DeleteFantasyCharacterById(1)).Returns(_fantasyCharacter);
+            //Act
+            var deletedFantasyCharacter = _fantasyCharacterController.DeleteFantasyCharacterById(1);
+            //Assert
+            deletedFantasyCharacter.Should().NotBeNull();
+            deletedFantasyCharacter.Should().BeOfType(typeof(ActionResult<FantasyCharacter>));
+            deletedFantasyCharacter.Value.Should().BeEquivalentTo(_fantasyCharacter);
         }
     }
 }
