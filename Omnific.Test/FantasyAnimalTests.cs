fcf7aac2f88f3f3dc8c21145af2d8d4e6fc0489f
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-26 17:39:26 +0200

    Fabio - Implementation FantasyAnimalTests

---
+++ Omnific.Test/FantasyAnimalTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Omnific.Model;
+using FluentAssertions;
+namespace Omnific.Test
+{
+    public class FantasyAnimalTests
+    {
+
+        [Test]
+        public void FantasyAnimal_Constructor_Test()
+        {
+            //Arrange/Act
+            var fantasyAnimal = new FantasyAnimal("Dracat", 5, 200, "Air/Earth", "Dracat: Dragon + Cat", "www.omnific.com/fantasyanimal/1", "12345678", "Fire", "Dragon", "Cat");
+
+            //Assert
+            fantasyAnimal.Should().NotBeNull();
+            fantasyAnimal.Should().BeOfType<FantasyAnimal>();
+            fantasyAnimal.Name.Should().Be("Dracat");
+            fantasyAnimal.Height.Should().Be(5);
+            fantasyAnimal.Weight.Should().Be(200);
+            fantasyAnimal.Habitat.Should().Be("Air/Earth");
+            fantasyAnimal.Description.Should().Be("Dracat: Dragon + Cat");
+            fantasyAnimal.Picture.Should().Be("www.omnific.com/fantasyanimal/1");
+            fantasyAnimal.APIKeyInventor.Should().Be("12345678");
+            fantasyAnimal.Powers.Should().Be("Fire");
+            fantasyAnimal.AnimalBaseAlpha.Should().Be("Dragon");
+            fantasyAnimal.AnimalBaseBeta.Should().Be("Cat");
+        }
+    }
+}
+
+ 
